352b0d9cdd8bf289f1229fb844fa490bf91aa151
GabrielLimaSC <gabrielsiqueira311@gmail.com>
2025-09-18 13:50:07 -0300

    Implementa funcionalidade de cadastro de produtos com campos para SKU, preço, nome e descrição

--- src/app/components/produtos/cadastro/index.tsx
+++ src/app/components/produtos/cadastro/index.tsx
@@ -1,6 +1,24 @@
+import { useState } from 'react'
 import { Layout } from '../../layout'
 
 export const CadastroProdutos: React.FC = () => {
+
+    const [sku, setSku] = useState('')
+    const [nome, setNome] = useState('')
+    const [preco, setPreco] = useState('')
+    const [descricao, setDescricao] = useState('')
+
+    const submit = () => {
+        const produto = {
+            sku,
+            nome,
+            preco,
+            descricao
+        }
+        
+        console.log(produto)
+    }
+
     return (
         <Layout titulo="Cadastro de Produtos">
             <div className="columns">
@@ -11,6 +29,8 @@ export const CadastroProdutos: React.FC = () => {
                     <div className="control is-expanded">
                         <input className="input placeholder-gray is-hovered has-text-black has-background-white" 
                         id="InputSku"
+                        value={sku}
+                        onChange={event => setSku(event.target.value)}
                         type="text" 
                         placeholder="Digite o SKU do produto"/>
                     </div>
@@ -22,6 +42,8 @@ export const CadastroProdutos: React.FC = () => {
                     <div className="control is-expanded">
                         <input className="input placeholder-gray is-hovered has-text-black has-background-white" 
                         id="InputPreco"
+                        value={preco}
+                        onChange={event => setPreco(event.target.value)}
                         type="text" 
                         placeholder="Digite o preço do produto"/>
                     </div>
@@ -36,6 +58,8 @@ export const CadastroProdutos: React.FC = () => {
                         <div className="control is-expanded">
                             <input className="input placeholder-gray is-hovered has-text-black has-background-white" 
                             id="InputNome"
+                            value={nome}
+                            onChange={event => setNome(event.target.value)}
                             type="text" 
                             
                             placeholder="Digite o nome do produto"/>
@@ -51,6 +75,8 @@ export const CadastroProdutos: React.FC = () => {
                     <div className="control is-expanded">
                         <textarea className="textarea placeholder-gray is-hovered has-text-black has-background-white" 
                         id="InputDescricao"
+                        value={descricao}
+                        onChange={event => setDescricao(event.target.value)}
                         placeholder="Digite a descrição do produto"></textarea>
                     </div>
                 </div>
@@ -59,7 +85,7 @@ export const CadastroProdutos: React.FC = () => {
             {/* Botões */}
             <div className="field is-grouped">
                 <div className="control">
-                    <button className="button is-link">Salvar</button>
+                    <button onClick={submit} className="button is-link">Salvar</button>
                 </div>
                 <div className="control">
                     <button className="button is-link is-light">Voltar</button>
